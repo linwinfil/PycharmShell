fee669d42d307207b10aa02d183925c2db890315
linmaoxin <linmaoxin@gomo.com>
2021-04-09 19:19:55 +0800

    ieee oui txt

--- oui_parse.py
+++ oui_parse.py
@@ -1,3 +1,20 @@
 import os
 import sys
 
+root = "D:/gomo/python/PycharmShell/ieee/"
+src_file_path = root + "oui.txt"
+
+dst_file_name = "nmap-mac-prefixes"
+dst_file_path = root + dst_file_name
+
+if os.path.exists(src_file_path):
+    with open(src_file_path) as fos:
+        line = fos.readline()
+        while line:
+            print(line)
+            line = fos.readline()
+
+# if os.path.exists(dst_file_path):
+#     os.remove(dst_file_path)
+# with open(dst_file_path, 'w') as fos:
+#     fos.write("x")
